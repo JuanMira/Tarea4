cf45af9138a668196a06133ab448c09bbcb11b2a
Juan Mira <juanmira20@hotmail.com>
2021-10-03 14:42:53 -0500

    Final commit

--- cart-items.js
+++ cart-items.js
@@ -1,21 +1,39 @@
-const tableContent = document.getElementById('table-content')
+const tableContent = document.getElementById("table-content");
 
 let subtotal = 0;
 
-$.getCartItems().forEach(data=>{
-    subtotal +=  parseInt(data.price) * parseInt(data.quantity) 
+const setCartItems = () => {
+  $.getCartItems().forEach((data, index) => {
+    subtotal += parseInt(data.price) * parseInt(data.quantity);
     tableContent.innerHTML += `
         <tr>
             <td><img src="${data.image}"/></td>
             <td>${data.quantity}</td>
             <td>$ ${data.price}</td>
-            <td>$ ${parseInt(data.price) * parseInt(data.quantity) }</td>
+            <td>$ ${parseInt(data.price) * parseInt(data.quantity)}</td>
+            <td><button class="remove" id="${index}" onclick="remove(${index})">x</button></td>
         </tr>
-    `    
-})
+    `;
+  });
+};
 
+setCartItems();
 
-const tableFoot = document.querySelector('.subtotal').innerHTML =  `    
+const test = () => alert("test");
+
+const tableFoot = (document.querySelector(".subtotal").innerHTML = `    
         <h4> Subtotal $ ${subtotal} </h4>
         <a href="./pago.html" class="button"> Pagar </a>
-`
+`);
+
+const remove = (id) => {
+  localStorage.setItem(
+    "cart",
+    JSON.stringify(
+      $.getCartItems().filter((element, index) => index !== parseInt(id))
+    )
+  );
+
+  $.setCartLength();
+  location.reload();
+};
